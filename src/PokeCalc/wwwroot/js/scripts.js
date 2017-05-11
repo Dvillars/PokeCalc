55767c598e82e634bec7713980b4eba72ff481d1
Derek Villars, Elise St Hilaire and Grinil Khanna <derek+elise+grinil@>
2017-05-11 14:18:53 -0700

    add img

---
+++ src/PokeCalc/wwwroot/js/scripts.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $(".first-pokemon").submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            data: { searchQuery: $(".first-chosen-pokemon").val() },
+            url: $(this).data('request-url'),
+            success: function (result) {
+                console.log(result);
+                $(".result").text("");
+                result.moves.forEach(function (thisMove) {
+                    $(".result").append("<h5 class='col-sm-3'>" + thisMove.move.name + "</h5>");
+                });
+                $(".img-result").html("<img class='center-image' src='" + result.sprites.front_shiny + "' />");
+                $(".result-name").html("<h1 class='text-center'>" + result.name + "</h1>");
+            }
+        });
+    });
+});
